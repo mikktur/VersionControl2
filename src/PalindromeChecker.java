3b42310be2d427384de78001e40a0eafe4676e87
Umut Uygur <umut.uygur30@gmail.com>
2024-02-19 16:41:56 +0200

    initial commit

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -1,20 +1,20 @@
-
+import java.util.*;
 
 
 public class PalindromeChecker {
 
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
+//this is where reader is created
+        Scanner reader = new Scanner(System.in);
+        String input = reader.nextLine();
 
         String transformed = transformInput(input);
-
+//this is where the "isPalindrome" is checked if it is true
         boolean isPalindrome = checkPalindrome(transformed);
         if (isPalindrome) {
-            System.out.println("The transformed input is a palindrome.");
+            System.out.println("The input(transformed) is a palindrome.");
         } else {
-            System.out.println("The transformed input is not a palindrome.");
+            System.out.println("The input(transformed) isn't a palindrome.");
         }
     }
 
